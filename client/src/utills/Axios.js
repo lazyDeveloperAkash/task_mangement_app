6138fc9ad35f086ba69870825bb6cc5920ecb284
lazyDeveloperAkash <lazydeveloperakash@gmail.com>
2024-10-23 00:26:07 +0530

    change endpoint

--- client/src/utills/Axios.js
+++ client/src/utills/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-    baseURL: 'http://localhost:5000/api', // Set your API base URL here
+    baseURL: 'https://task-mangement-4nhh43v6x-akashs-projects-f9f05adc.vercel.app/api', // Set your API base URL here
     withCredentials: true
 });
