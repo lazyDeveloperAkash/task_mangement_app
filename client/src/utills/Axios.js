5e8e78bcf98ed6cb6d47247665a937f4647c6714
lazyDeveloperAkash <138031629+lazyDeveloperAkash@users.noreply.github.com>
2024-10-23 07:33:09 +0530

    Update Axios.js
    set api base url

--- client/src/utills/Axios.js
+++ client/src/utills/Axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const Axios = axios.create({
-    baseURL: 'https://task-mangement-4nhh43v6x-akashs-projects-f9f05adc.vercel.app/api', // Set your API base URL here
+    baseURL: 'https://task-mangement-app-drab.vercel.app//api',
     withCredentials: true
 });
